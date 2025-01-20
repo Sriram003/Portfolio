3a70671c5e996eef018df752c4e6969ed4d3e954
Sriram003 <sriramvedantam@gmail.com>
2025-01-20 12:10:27 +0530

    Update image paths for assets folder

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ const spaceTexture = new THREE.TextureLoader().load('space_image.jpg');
 scene.background = spaceTexture;
 
 // Avatar
-const sriTexture = new THREE.TextureLoader().load('sri.png');
+const sriTexture = new THREE.TextureLoader().load('assets/images/sri.png');
 
 const sri = new THREE.Mesh(
     new THREE.BoxGeometry(3,3,3),
@@ -68,8 +68,8 @@ const sri = new THREE.Mesh(
 scene.add(sri);
 
 // Moon
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('assets/images/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('assets/images/normal.jpg');
 
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(3,32,32),

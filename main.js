8b07afb041cda4c7a93bc96ef7b276cbc1729819
Sriram003 <sriramvedantam@gmail.com>
2025-01-29 11:30:22 +0530

    Update portfolio project to fix GitHub Pages issues

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 // import * as THREE from './node_modules/three/build/three.module.js';
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
